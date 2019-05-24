53c023455498c6b39446d42587d21fed1fe76963
sdk android <android@PC43MAC.local>
2019-05-24 17:01:38 -0500

    Correctly moved some code to respect the MVMV model. Made some pop up appear on invalid username or password.

--- Javeriana-Cali/Javeriana-Cali/Views/LoginView.xaml.cs
+++ Javeriana-Cali/Javeriana-Cali/Views/LoginView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using JaverianaCali.Models;
 using JaverianaCali.ViewModels;
 using Xamarin.Forms;
 
@@ -7,14 +9,23 @@ namespace JaverianaCali.Views
 {
     public partial class LoginView : ContentPage
     {
-        LoginViewModel context;
+        private LoginViewModel context;
 
         public LoginView()
         {
             InitializeComponent();
-
-            context = new LoginViewModel(this);
+            context = new LoginViewModel();
             BindingContext = context;
+        }
+
+        private void ClickedLogin(object sender, EventArgs args) {
+            UserModel user = context.AttemptLogin();
+            if (user == null) {
+                // Invalid user attempted to log in :)
+                DisplayAlert("Autenticacion fallida", "Datos de inicio incorrectos", "OK");
+            } else {
+                Navigation.PushAsync(new CoursesView(user));
+            }
         }
     }
 }
